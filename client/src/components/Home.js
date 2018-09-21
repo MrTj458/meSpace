1adad38e0538e477babd9aa7027d11bace24da29
MrTj458 <mrtj458@gmail.com>
2018-09-21 15:14:14 -0600

    Home page now shows a list of all users

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,12 +1,31 @@
-import React, { Component } from 'react';
-import { Header } from 'semantic-ui-react';
+import React, { Fragment } from 'react'
+import { Header, List } from 'semantic-ui-react'
+import axios from 'axios'
 
-class Home extends Component {
+class Home extends React.Component {
+  state = {users: []}
+
+  componentDidMount() {
+    axios.get('/api/users')
+      .then(res => this.setState({users: res.data}))
+  }
+
+  clickUser = (id) => {
+    this.props.history.push(`/users/${id}`)
+  }
+
   render() {
     return (
-      <Header as='h1' textAlign='center'>Home Component</Header>
-    );
+      <Fragment>
+        <Header as="h1">Users:</Header>
+        <List bulleted>
+        {this.state.users.map(user =>
+          <List.Item key={user.id} onClick={() => this.clickUser(user.id)}>{user.nickname}</List.Item>
+        )}
+        </List>
+      </Fragment>
+    )
   }
 }
 
-export default Home;
+export default Home
